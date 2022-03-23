8585309f922c1f11371827204e50a61cbf8b34df
Alexandru Ichim <alex@matemates.com>
2022-03-23 11:39:59 -0300

    2021 may june july

--- photosheet.php
+++ photosheet.php
@@ -3,10 +3,11 @@
 $site_title = "Photographs of Roadside America";
 $site_desc = "by John Margolies";
 $site_style = "style.css";
-$img_folder = "img/john-margolies";
-$allowed_types = ["gif","jpg","jpeg","png","webp"];
+$img_folder = "img";
+$allowed_types = ["gif", "jpg", "jpeg", "png", "webp"];
 
-function create_slug($string) {
+function create_slug($string)
+{
   $string = strtolower($string);
   $string = strip_tags($string);
   $string = stripslashes($string);
@@ -18,21 +19,22 @@ function create_slug($string) {
 
 $dimg = opendir($img_folder);
 
-while($img_file = readdir($dimg)) {
-  if(in_array(strtolower(end(explode('.',$img_file))),$allowed_types))
-  {$a_img[] = $img_file;} 
+while ($img_file = readdir($dimg)) {
+  if (in_array(strtolower(end(explode('.', $img_file))), $allowed_types)) {
+    $a_img[] = $img_file;
+  }
 }
 
-if(is_array($a_img)) sort($a_img, SORT_STRING | SORT_FLAG_CASE | SORT_NATURAL);
+if (is_array($a_img)) sort($a_img, SORT_STRING | SORT_FLAG_CASE | SORT_NATURAL);
 
 $totimg = count($a_img);
 
-for($x = 0; $x < $totimg; $x++) {
-  
-  $file_name = pathinfo($a_img[$x], PATHINFO_FILENAME); 
+for ($x = 0; $x < $totimg; $x++) {
+
+  $file_name = pathinfo($a_img[$x], PATHINFO_FILENAME);
   $file_slug = create_slug($file_name);
-  
-  $size = getimagesize($img_folder.'/'.$a_img[$x]);
+
+  $size = getimagesize($img_folder . '/' . $a_img[$x]);
   $width = $size[0];
   $height = $size[1];
   $aspect = $height / $width;
@@ -40,29 +42,30 @@ for($x = 0; $x < $totimg; $x++) {
   else $orientation = 'landscape';
 
   $grid .= '
-  <figure class="'.$orientation.'">
-    <a href="#'.$file_slug.'" id="'.$file_slug.'-thumb">
-      <img class="open" loading="lazy" width="'.$width.'" height="'.$height.'" src="'.$img_folder.'/'.$a_img[$x].'" alt="'.$file_name.'">
+  <figure class="' . $orientation . '">
+    <a href="#' . $file_slug . '" id="' . $file_slug . '-thumb">
+      <img class="open" loading="lazy" width="' . $width . '" height="' . $height . '" src="' . $img_folder . '/' . $a_img[$x] . '" alt="' . $file_name . '">
     </a>
-    <figcaption>'.$file_name.'</figcaption>
+    <figcaption>' . $file_name . '</figcaption>
   </figure>
   ';
-  
+
   $lightbox .= '
-  <figure tabindex="0" id="'.$file_slug.'" class="'.$orientation.'">
-    <a tabindex="-1" href="#'.$file_slug.'" class="image">
-      <img loading="lazy" width="'.$width.'" height="'.$height.'" src="'.$img_folder.'/'.$a_img[$x].'" alt="'.$file_name.'">
+  <figure tabindex="0" id="' . $file_slug . '" class="' . $orientation . '">
+    <a tabindex="-1" href="#' . $file_slug . '" class="image">
+      <img loading="lazy" width="' . $width . '" height="' . $height . '" src="' . $img_folder . '/' . $a_img[$x] . '" alt="' . $file_name . '">
     </a>
-    <a tabindex="-1" href="#'.$file_slug.'-thumb" class="close">Close</a>
+    <a tabindex="-1" href="#' . $file_slug . '-thumb" class="close">Close</a>
   </figure>
   ';
 }
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">     
+  <meta name="viewport" content="width=device-width">
   <title><?php echo $site_title; ?></title>
   <meta name="description" content="<?php echo $site_desc; ?>">
   <meta property="og:title" content="<?php echo $site_title; ?>">
@@ -72,6 +75,7 @@ for($x = 0; $x < $totimg; $x++) {
     <?php echo file_get_contents($site_style); ?>
   </style>
 </head>
+
 <body>
   <main>
     <h1><?php echo $site_title; ?> <span><?php echo $site_desc; ?></span></h1>
@@ -87,43 +91,43 @@ for($x = 0; $x < $totimg; $x++) {
       <?php echo $grid; ?>
     </div>
     <div class="lightbox">
-      <?php echo $lightbox; ?>   
-      <div class="counter"></div>        
-    </div> 
+      <?php echo $lightbox; ?>
+      <div class="counter"></div>
+    </div>
   </main>
   <footer>
     This <a target="_blank" rel="noopener" href="http://github.com/cadars/photosheet">photo sheet</a> was generated on <?php echo date("F j, Y"); ?>
   </footer>
   <script>
-  // show lightbox
-  document.addEventListener('click', function(event) {
-    if (!event.target.matches('.open')) return;
-    document.querySelector('body').classList.add('fixed')
-  }, false);
-
-  document.addEventListener(
-    "keydown", (e) => {
-      if (e.keyCode == 13) {
-        document.activeElement.click();
-        document.querySelector('body').classList.add('fixed')
-      }
+    // show lightbox
+    document.addEventListener('click', function(event) {
+      if (!event.target.matches('.open')) return;
+      document.querySelector('body').classList.add('fixed')
     }, false);
 
-  // hide lightbox
-  document.addEventListener('click', function(event) {
-    if (!event.target.matches('.close')) return;
-    document.querySelector('body').classList.remove('fixed')
-  }, false);
+    document.addEventListener(
+      "keydown", (e) => {
+        if (e.keyCode == 13) {
+          document.activeElement.click();
+          document.querySelector('body').classList.add('fixed')
+        }
+      }, false);
 
-  // esc key to close
-  document.addEventListener(
-    "keydown", (e) => {
-      if (e.keyCode == 27) {
-        document.activeElement.querySelector('.close').click();
-        document.querySelector('body').classList.remove('fixed');
-      }
+    // hide lightbox
+    document.addEventListener('click', function(event) {
+      if (!event.target.matches('.close')) return;
+      document.querySelector('body').classList.remove('fixed')
     }, false);
 
+    // esc key to close
+    document.addEventListener(
+      "keydown", (e) => {
+        if (e.keyCode == 27) {
+          document.activeElement.querySelector('.close').click();
+          document.querySelector('body').classList.remove('fixed');
+        }
+      }, false);
   </script>
 </body>
+
 </html>
